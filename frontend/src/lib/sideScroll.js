d9a1d7ea696d495b95a02dda68e6684ea66d9760
jamesikjo <jamesikjo@gmail.com>
2022-06-17 21:25:36 -0700

    Add comment

--- frontend/src/lib/sideScroll.js
+++ frontend/src/lib/sideScroll.js
@@ -1,4 +1,5 @@
 //For navigating work photo gallery thumbnails
+//Shift thumbnails on smaller viewports when thumbnails extend past screen size
 const sideScroll = (element, speed, distance, step) => {
   let scrollAmount = 0;
   console.log(element.scrollLeft);
