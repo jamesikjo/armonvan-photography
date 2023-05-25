51e91a0e95b29b65a20ff761dfe0202c29cd4a9c
jamesikjo <jamesikjo@gmail.com>
2023-05-24 17:54:52 -0700

    Remove corssOrigin

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -10,11 +10,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             rel="preload"
             href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
