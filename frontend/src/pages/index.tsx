aeceaf602d1dec6d673f40840c24aed603007c9a
jamesikjo <jamesikjo@gmail.com>
2023-05-24 17:59:24 -0700

    Refactor code and add delay for scroll pos. calculation

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box, Container, useTheme, useMediaQuery } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import NoSsr from "@mui/material/NoSsr";
 import Layout from "../Layout";
 import {
@@ -29,11 +29,6 @@ const homeSectionImages = {
 const Home = () => {
   const [modalState, setModalState] = useState({ open: false, image: "" });
 
-  const theme = useTheme();
-  const isMd = useMediaQuery(theme.breakpoints.up("md"), {
-    defaultMatches: true,
-  });
-
   useEffect(() => {
     const jarallaxInit = async () => {
       const jarallaxElems = document.querySelectorAll(".jarallax");
@@ -46,7 +41,7 @@ const Home = () => {
     };
 
     jarallaxInit();
-  });
+  }, []);
 
   const scrollTo = (id: string) => {
     setTimeout(() => {
@@ -60,7 +55,7 @@ const Home = () => {
         top: htmlElement.offsetTop,
         behavior: "smooth",
       });
-    });
+    }, 75);
   };
 
   const toggleModal = (image: string) => {
@@ -76,121 +71,114 @@ const Home = () => {
     top: 0,
     left: 0,
     width: "100%",
-    height: "100%",
+    height: "100vh",
     zIndex: -1,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center center",
     backgroundImage: `url(${bgImg})`,
-    filter: "brightness(0.70)",
+    filter: "brightness(0.75)",
     backgroundColor: "#10100D",
   });
 
   return (
     <Layout colorInvert={true}>
-      <>
-        <Box
-          minHeight="100vh"
-          display="flex"
-          alignItems="center"
-          bgcolor="rgba(0, 0, 0, 0.45)"
-          mt={isMd ? 0 : -14}
-          pt={isMd ? 0 : 14}
-        >
-          <Container maxWidth="lg">
-            <Box display="flex" flexDirection="column" alignItems="center">
-              <Box sx={styles(homeSectionImages.welcome)} />
-              <Welcome />
-              <Box mt={4}>
-                <NoSsr>
-                  <Box
-                    component="svg"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="#fff"
-                    width={{ xs: 30, sm: 35 }}
-                    height={{ xs: 30, sm: 35 }}
-                    onClick={() =>
-                      scrollTo("agency__portfolio-item--js-scroll")
-                    }
-                    sx={{ cursor: "pointer" }}
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </Box>
-                </NoSsr>
-              </Box>
+      <Box
+        minHeight="100vh"
+        display="flex"
+        alignItems="center"
+        bgcolor="rgba(0, 0, 0, 0.45)"
+      >
+        <Container maxWidth="lg">
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <Box sx={styles(homeSectionImages.welcome)} />
+            <Welcome />
+            <Box>
+              <NoSsr>
+                <Box
+                  component="svg"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#fff"
+                  width={{ xs: 30, sm: 35 }}
+                  height={{ xs: 30, sm: 35 }}
+                  onClick={() => scrollTo("portfolio-item--js-scroll")}
+                  sx={{ cursor: "pointer" }}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </Box>
+              </NoSsr>
             </Box>
-          </Container>
-        </Box>
+          </Box>
+        </Container>
+      </Box>
+      <Box
+        className="jarallax"
+        data-jarallax
+        data-speed="0.2"
+        position="relative"
+        minHeight="100vh"
+        display="flex"
+        alignItems="center"
+        bgcolor="#0A0A08"
+        id="portfolio-item--js-scroll"
+      >
         <Box
-          className="jarallax"
-          data-jarallax
-          data-speed="0.2"
-          position="relative"
-          minHeight="100vh"
-          display="flex"
-          alignItems="center"
-          id="agency__portfolio-item--js-scroll"
-        >
-          <Box
-            className="jarallax-img"
-            sx={styles(homeSectionImages.landscapes)}
-          />
-          <Container maxWidth="lg">
-            <Landscapes
-              toggleModal={() => toggleModal(homeSectionImages.landscapes)}
-            />
-          </Container>
-        </Box>
-        <Box
-          className="jarallax"
-          data-jarallax
-          data-speed="0.2"
-          position="relative"
-          minHeight="100vh"
-          display="flex"
-          alignItems="center"
-        >
-          <Box className="jarallax-img" sx={styles(homeSectionImages.sounds)} />
-          <Container maxWidth="lg">
-            <Sounds toggleModal={() => toggleModal(homeSectionImages.sounds)} />
-          </Container>
-        </Box>
-        <Box minHeight="100vh" display="flex" alignItems="center">
-          <Container>
-            <Bio />
-          </Container>
-        </Box>
-        <Box
-          className="jarallax"
-          data-jarallax
-          data-speed="0.2"
-          position="relative"
-          minHeight="100vh"
-          display="flex"
-          alignItems="center"
-          id="agency__portfolio-item--js-scroll"
-        >
-          <Box
-            className="jarallax-img"
-            sx={styles(homeSectionImages.wildlife)}
-          />
-          <Container maxWidth="lg">
-            <WildLife
-              toggleModal={() => toggleModal(homeSectionImages.wildlife)}
-            />
-          </Container>
-        </Box>
-        <PhotoModal
-          image={modalState.image}
-          open={modalState.open}
-          handleModalClose={() => toggleModal("")}
+          className="jarallax-img"
+          sx={styles(homeSectionImages.landscapes)}
         />
-      </>
+        <Container maxWidth="lg">
+          <Landscapes
+            toggleModal={() => toggleModal(homeSectionImages.landscapes)}
+          />
+        </Container>
+      </Box>
+      <Box
+        className="jarallax"
+        data-jarallax
+        data-speed="0.2"
+        position="relative"
+        minHeight="100vh"
+        display="flex"
+        alignItems="center"
+        bgcolor="#0A0A08"
+      >
+        <Box className="jarallax-img" sx={styles(homeSectionImages.sounds)} />
+        <Container maxWidth="lg">
+          <Sounds toggleModal={() => toggleModal(homeSectionImages.sounds)} />
+        </Container>
+      </Box>
+      <Box minHeight="100vh" display="flex" alignItems="center">
+        <Container>
+          <Bio />
+        </Container>
+      </Box>
+      <Box
+        className="jarallax"
+        data-jarallax
+        data-speed="0.5"
+        position="relative"
+        minHeight="100vh"
+        display="flex"
+        alignItems="center"
+        bgcolor="#0A0A08"
+      >
+        <Box className="jarallax-img" sx={styles(homeSectionImages.wildlife)} />
+        <Container maxWidth="lg">
+          <WildLife
+            toggleModal={() => toggleModal(homeSectionImages.wildlife)}
+          />
+        </Container>
+      </Box>
+      <PhotoModal
+        image={modalState.image}
+        open={modalState.open}
+        handleModalClose={() => toggleModal("")}
+      />
     </Layout>
   );
 };
